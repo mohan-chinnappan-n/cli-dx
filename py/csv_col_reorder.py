bbd2b4b8d0f55f5a6c43df52c234d73dd4701d7a
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2021-02-06 16:04:46 -0500

    col reorder added

---
+++ py/csv_col_reorder.py
@@ -0,0 +1,31 @@
+### csv_col_reorder.py
+# Reorders a given CSV with the requested columns
+
+# Example usage:
+# python csv_col_reorder.py in.csv cols.txt out.csv
+
+
+
+import csv
+import argparse
+
+parser = argparse.ArgumentParser(description='Reorder columns in the given CSV file')
+parser.add_argument('infile', metavar='i', type=str, help='Input CSV File')
+parser.add_argument('colsfile', metavar='c', type=str, help='File with the columns needed')
+parser.add_argument('outfile', metavar='o', type=str, help='Output CSV File')
+
+# read args
+args = parser.parse_args()
+inputFile = args.infile
+colsFile = args.colsfile
+outFile = args.outfile
+
+# let us read the cols needed info
+with open(colsFile, 'r') as cf:
+    colData = cf.read().strip() 
+    fieldnames =  colData.split(",")
+with open(inputFile, 'r') as infile, open(outFile, 'w') as outfile:
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in csv.DictReader(infile):
+        writer.writerow(row)
---
+++ py/howto.md
@@ -0,0 +1,50 @@
+# Python script to reorder CSV file columns
+
+
+## Inputs
+```
+$ cat cols.txt 
+City,Name,State,Age
+```
+
+```
+$ cat in.csv 
+Name,Age,City,State
+Ken Thompson,78,Boston,MA
+Dennis Ritchie,75,New York,NY
+Johnny Seed,87,Nashua,NH
+```
+
+## Script
+
+### Usage
+```
+$ python3 csv_col_reorder.py in.csv -h
+usage: csv_col_reorder.py [-h] i c o
+
+Reorder columns in the given CSV file
+
+positional arguments:
+  i           Input CSV File
+  c           File with the columns needed
+  o           Output CSV File
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
+### Demo
+
+``
+$ python csv_col_reorder.py in.csv cols.txt output.csv
+```
+
+## Output
+
+```
+$ cat output.csv 
+City,Name,State,Age
+Boston,Ken Thompson,MA,78
+New York,Dennis Ritchie,NY,75
+Nashua,Johnny Seed,NH,87
+```
+
