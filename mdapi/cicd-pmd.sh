70dfa4da02d9b09b5826371bf90f1e22f4589e8c
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2023-01-25 05:29:17 -0500

    pmd update

--- mdapi/cicd-pmd.sh
+++ mdapi/cicd-pmd.sh
@@ -1,13 +1,23 @@
 # Script for pipeline
-# Bails out if count of P1 and P2 are not zero
+# Bails out if count of P1,P2 and P3 are not zero
 # -----------------------------------------
 
+#----- configure the following to meet your needs ----
+RULESET=~/.pmd/apex_ruleset.xml
+MSG="Number of P1,P2 and P3 issues are"
+THRESHOLD=4
+CODE=/tmp/apex/unpackaged/classes/
+#-----------------------------------------------------
+
 # Run the pmd
-pmd-run.sh pmd -R ~/.pmd/apex_ruleset.xml -d /tmp/deployment/unpackaged/classes/  -f csv > results.csv
+echo pmd-run.sh pmd -R  $RULESET -d $CODE  -f csv > results.csv
+pmd-run.sh pmd -R  $RULESET -d $CODE  -f csv > results.csv
+
 
 # query the results using SQL
-echo 'SELECT COUNT(*) AS CNT   FROM CSV("results.csv", {headers:true}) WHERE Priority < 3' > q.sql
-sfdx mohanc:data:query:sql -q q.sql -d results.csv > out.json
+echo "SELECT COUNT(*) AS CNT   FROM CSV(\"results.csv\", {headers:true}) WHERE Priority < $THRESHOLD" > q.sql
+cat q.sql
+sfdx mohanc:data:query:sql -q q.sql  > out.json
 
 # check for the errors
 nerrors=`sfdx mohanc:data:jq -f  '.[].CNT'  -i out.json`
@@ -15,7 +25,8 @@ echo "nerrors: $nerrors"
 
 if [ "$nerrors" != 0 ]
 then
-  echo "Number of P1 and P2 issues are:  $nerrors. Stopping the deployment!"
+  echo "$MSG:  $nerrors. Stopping the deployment!"
   exit 2
 fi
 echo "Continue the deployment..."
+
