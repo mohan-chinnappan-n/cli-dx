5d51c18fc12445ff9a0650ae070b5faa60390b9d
mohan chinnappan <mohan.chinnappan.n@gmail.com>
2023-11-30 06:45:52 -0500

    jwt cleanup

--- jwt/form_login_urls.sh
+++ jwt/form_login_urls.sh
@@ -13,6 +13,8 @@
 # Usage: bash form_login_urls.sh <csv_file_path> <keyfile_path> <consumerKey> <output_csv>
 # bash form_login_urls.sh users.csv ~/.keys/autoDeploy.key '3MVG9Kip4IKAJUNK4gZDQcUI7albyrfHKO6yYqMW09rBf5JiiqNOx3jgIZdPvBWargyvDKjnXlwrZ4uhR6wmL' loginUrls.csv
 
+# use this app to test with access_token and instance_url:
+# https://mohan-chinnappan-n5.github.io/sf/query/app.html
 #----------------------------------------------------------------------------
 
 
@@ -40,14 +42,18 @@ while IFS=',' read -r username; do
         json_output=$(sfdx mohanc:hello:myorg -u "$username")
 
         # Extract Authorization string and parse AccessToken
+        # e.g: 
+        # cat header.json| jq -r '.Authorization' |  awk '{print $2}' 
+        # access_token=`cat header.json| jq -r '.Authorization' |  awk '{print $2}'`
         authorization=$(echo "$json_output" | jq -r '.Authorization')
         access_token=$(echo "$authorization" | awk '{print $2}')
 
         # Extract InstanceUrl from JSON output
+        # instance_url=`cat header.json | jq -r '.instanceUrl'`
         instance_url=$(echo "$json_output" | jq -r '.instanceUrl')
 
         # Form the URL and write to output file
-        url="$instance_url/secur/frontdoor.jsp?sid=$access_token"
+        url="$instance_url/secur/frontdoor.jsp?sid=\"$access_token\""
         
         # Append user data to the array
         user_data+=("$username,$url")
